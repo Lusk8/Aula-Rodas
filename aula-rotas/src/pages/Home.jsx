148d5fc51082cd5fe8a7041cb6914f052642700d
Lusk8 <128000584+Lusk8@users.noreply.github.com>
2023-05-10 22:08:42 -0300

    Add files via upload

---
+++ aula-rotas/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+    return(
+        <h1>Ola mundo da home</h1>
+    )
+}
+
+export default Home;
---
+++ aula-rotas/src/pages/Sobre.jsx
@@ -0,0 +1,7 @@
+function Sobre(){
+    return(
+        <h1>Ola mundo sobre</h1>
+    )
+}
+
+export default Sobre;
